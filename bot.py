05c05d390c021065ec4ac9c8790eb254c94b7ce8
medycynka <tyrranozaur08@gmail.com>
2021-05-31 17:38:38 +0200

    removed token

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
 import discord
 import os
-
 from discord.ext import commands
 
-# Read the Data files and store them in a variable
-TokenFile = open("data/Token.txt", "r")  # Make sure to paste the token in the txt file
-TOKEN = TokenFile.read()
 
+TokenFile = open("data/Token.txt", "r")
+TOKEN = TokenFile.read()
 OWNERID = 729461654189899776
 
-# Define "bot"
+
 bot = commands.Bot(command_prefix="!", case_insensitive=True)
 
 
-# Let us Know when the bot is ready and has started
 @bot.event
 async def on_ready():
     print("Bot is ready")
 
 
-# A simple and small ERROR handler
 @bot.event
 async def on_command_error(ctx, error):
     embed = discord.Embed(
@@ -36,10 +32,8 @@ async def on_command_error(ctx, error):
         raise error
 
 
-# Load command to manage our "Cogs" or extensions
 @bot.command()
 async def load(ctx, extension):
-    # Check if the user running the command is actually the owner of the bot 
     if ctx.author.id == OWNERID:
         bot.load_extension(f'Cogs.{extension}')
         await ctx.send(f"Enabled the Cog!")
@@ -47,10 +41,8 @@ async def load(ctx, extension):
         await ctx.send(f"You are not cool enough to use this command")
 
 
-# Unload command to manage our "Cogs" or extensions
 @bot.command()
 async def unload(ctx, extension):
-    # Check if the user running the command is actually the owner of the bot 
     if ctx.author.id == OWNERID:
         bot.unload_extension(f'Cogs.{extension}')
         await ctx.send(f"Disabled the Cog!")
@@ -58,10 +50,8 @@ async def unload(ctx, extension):
         await ctx.send(f"You are not cool enough to use this command")
 
 
-# Reload command to manage our "Cogs" or extensions
 @bot.command(name="reload")
 async def reload_(ctx, extension):
-    # Check if the user running the command is actually the owner of the bot 
     if ctx.author.id == OWNERID:
         bot.reload_extension(f'Cogs.{extension}')
         await ctx.send(f"Reloaded the Cog!")
@@ -69,7 +59,6 @@ async def reload_(ctx, extension):
         await ctx.send(f"You are not cool enough to use this command")
 
 
-# Automatically load all the .py files in the Cogs folder
 for filename in os.listdir('Cogs'):
     if filename.endswith('.py'):
         try:
@@ -77,5 +66,5 @@ for filename in os.listdir('Cogs'):
         except Exception:
             raise Exception
 
-# Run our bot
-bot.run(str(TOKEN))  # Make sure you paste the CORRECT token in the "./data/Token.txt" file
+
+bot.run(str(TOKEN))
